3f1593663e517e15be48b842fb9b51d8bf16caac
dmorensa <dmorensa@everis.com>
2017-04-04 16:48:40 +0200

    04/04/2017 DM - Inicio Proyecto Revisar Documento

---
+++ ckeckingDocuments/src/main/java/com/ckeckingdocuments/CkeckingDocumentsApplication.java
@@ -0,0 +1,12 @@
+package com.ckeckingdocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkeckingDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkeckingDocumentsApplication.class, args);
+	}
+}
